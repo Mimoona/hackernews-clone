5f25ad9b60cb42ba8348d0edf27eb0635e3b7594
Mimoona <memoona.raheel8@gmail.com>
2021-12-20 14:08:09 +0100

    showing news list

--- src/App.js
+++ src/App.js
@@ -2,28 +2,42 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 //import NewsList from './components/NewsList'
 import axios from 'axios';
-import mapTime from './components/mapTime';
+//import _ from "lodash";
+//import PaginationItems from './components/PaginationItems'
+import ReactPaginate from "react-paginate"
+import Header from './components/Header'
+import Footer from "./components/Footer"
 
 function App() {
   
-  const[item, setItem] = useState([])
+  const[items, setItems] = useState([])
   const[loading, setLoading]= useState(false)
   const [querie, setQuerie] = useState('')
   const [search, setSearch] = useState('')
-  const URL = `http://hn.algolia.com/api/v1/search?query=${search}`
+  const[pageCount,setPageCount]=useState(0)
+  let currentPage = 1;
+  const itemsPerPage = 10
+  
+  const URL = `http://hn.algolia.com/api/v1/search?query=${search}&page=${currentPage}&hitsPerPage=${itemsPerPage}`
+  //const pageSize = 5;
+  // const [paginatedItems, setPaginatedItems] = useState()
+  
 
   useEffect(() => {
+   
     fetchData()
   }, [search])
   
   const fetchData = () => {
-     axios.get(URL)
-    .then(res =>{
-      setItem(res.data)
-      setLoading(true)
-    })
-    .catch(err => console.log(err))
-  }
+    axios.get(URL)
+   .then(res =>{
+     setItems(res.data)
+     // setPaginatedItems(_(items).slice(0).take(pageSize).value())
+     setPageCount(Math.ceil(items.length/itemsPerPage))
+     setLoading(true)
+   })
+   .catch(err => console.log(err))
+ }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -31,10 +45,17 @@ function App() {
     setQuerie('')
   }
 
-console.log(item)
+  const handlePageClick = (items) => {
+    console.log(items.selected)
+  
+  }
+  
+console.log(items)
   return (
     <div >
-        <form onSubmit={handleSubmit}>
+      <Header />
+      <br></br>
+        <form style={{justify:"center"}} class="search" onSubmit={handleSubmit}>
           <input value={querie} onChange={e => setQuerie(e.target.value)}/>
           <button type='submit'>
             Search
@@ -44,31 +65,44 @@ console.log(item)
             <div>
 
             <ol >
-                {item.hits.map(eachObj => {
-                return(  <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
-
+                {items.hits.map(hit => {
+                return(  
+                <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
                 <div>
-                <h1><a className="title" href={eachObj.url}>{eachObj.title}</a></h1>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.points} points</a></p>
-                <p className='unterTitle'><a href={eachObj.url}>by {eachObj.author} </a> </p>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.num_comments} comments</a></p>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.created_at} </a></p>
-                <p className='unterTitle'><a href={eachObj.url}>{mapTime(eachObj.created_at_i)} </a></p> 
-            
-                
+                <h1><a  className="title"  href={hit.url}>{hit.title}</a></h1>
+                <p  className="subHeading" ><a href={hit.url}>{hit.points} points</a> <a href={hit.url}>by {hit.author} </a> <a href={hit.url}>{hit.num_comments} comments</a> <a href={hit.url}>{hit.created_at} </a>
+                </p>
+                {/* <p><a href={eachObj.url}>{mapTime(eachObj.created_at_i)} </a></p>  */}
                 </div> 
                 </li>
                )})} 
              </ol>
             </div>
           )
-
-        
           : <h1>Wait, It's loading.....</h1>
         }
+      <ReactPaginate
+      previousLabel={"previous"}
+      nextLabel={"next"}
+      breakLabel={"..."}
+      pageCount={pageCount}
+      marginPagesDisplayed={2}
+      onPageChange={handlePageClick}
+      containerClassName={"pagination justify-content-center"}
+      pageClassName={"page-item"}
+      pageLinkClassName= {"page-link"}
+      previousLinkClassName={"page-link"}
+      nextLinkClassName={"page-link"}
+      breakLinkClassName={"page-Link"}
+      activeClassName={"onPageActive"}
+      
+      />
+      
+      {/* <Footer/> */}
+     
     </div>
      
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Logo from './Logo.png'
+
+
+
+function Header() {
+    return (
+      <header>
+                <div className="nav-link">
+                    <img id="logo" src={Logo} alt="logo" />
+                    {/* <a href="#"> LOGIN </a>  */}
+                    <a className="hLinks" href="https://www.google.com/">BREAKING </a>
+                    <a className="hLinks" href="https://www.google.com/">OL'STUFF </a>
+                    <a className="hLinks" href="https://www.google.com/">COMMENTS </a>
+                    <a className="hLinks" href="https://www.google.com/">JOBS </a>
+                    <a className="hLinks" href="https://www.google.com/">SUBMIT </a>
+                    <a className="hLinks" href="https://www.google.com/">LOGIN </a>
+                </div>
+      </header>
+    );
+}
+  
+export default Header
