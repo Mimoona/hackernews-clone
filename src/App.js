cc9bd11328d62c1083dc2b8b1a865ba032bc3c55
Mimoona <memoona.raheel8@gmail.com>
2021-12-22 10:49:34 +0100

    tried my best

--- src/App.js
+++ src/App.js
@@ -2,70 +2,109 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 //import NewsList from './components/NewsList'
 import axios from 'axios';
-import mapTime from './components/mapTime';
+//import mapTime from './components/mapTime';
+import ReactPaginate from 'react-paginate'
 
 function App() {
   
-  const[item, setItem] = useState([])
+  const[hits, setHits] = useState([])
   const[loading, setLoading]= useState(false)
   const [querie, setQuerie] = useState('')
   const [search, setSearch] = useState('')
-  const URL = `http://hn.algolia.com/api/v1/search?query=${search}`
 
-  useEffect(() => {
-    fetchData()
-  }, [search])
-  
+  // Declarations for Pagination
+  const[pageNumber,setPageNumber]= useState(0)
+  const hitsPerPage = 10
+  const hitsVisited = pageNumber * hitsPerPage
+  const pageCount = Math.ceil(hits.length / hitsPerPage)
+
+  //const URL = `http://hn.algolia.com/api/v1/search?query=${search}&tags=front_page`
+  // const URL = `http://hn.algolia.com/api/v1/search?query=${search}&page=2`
+  const URL = `http://hn.algolia.com/api/v1/search?query=${search}&hitsPerPage=10`
+
   const fetchData = () => {
-     axios.get(URL)
+    axios
+    .get(URL)
     .then(res =>{
-      setItem(res.data)
-      setLoading(true)
+      setHits(res.data.hits.slice(0,101));
+      setLoading(true);
     })
     .catch(err => console.log(err))
   }
 
+  useEffect(() => {
+      fetchData();
+  }, [search])
+
+
+  const handlePageChange = ({selected}) => {
+    setPageNumber(selected)
+  }
+  
   const handleSubmit = (e) => {
     e.preventDefault()
     setSearch(querie)
     setQuerie('')
   }
 
-console.log(item)
-  return (
-    <div >
-        <form onSubmit={handleSubmit}>
-          <input value={querie} onChange={e => setQuerie(e.target.value)}/>
-          <button type='submit'>
-            Search
-          </button>
-        </form>
-        {loading ? (
-            <div>
-            <ol >
-                {item.hits.map(eachObj => {
-                return(  <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
+console.log(hits)
+return (
+  <div >
+    {/* <Header /> */}
+    <br></br>
+      <form style={{textAlign:"center"}} class="search" onSubmit={handleSubmit}>
+        <input value={querie} onChange={e => setQuerie(e.target.value)}/>
+        <button type='submit'>
+          Search
+        </button>
+      </form>
+      {loading ? (
+          <div>
 
+          <ol >
+              {hits && hits
+              .slice(hitsVisited, hitsVisited + hitsPerPage)
+              .map(hit => {
+              return(  
+              <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
                 <div>
-                <h1><a className="title" href={eachObj.url}>{eachObj.title}</a></h1>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.points} points</a></p>
-                <p className='unterTitle'><a href={eachObj.url}>by {eachObj.author} </a> </p>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.num_comments} comments</a></p>
-                <p className='unterTitle'><a href={eachObj.url}>{eachObj.created_at} </a></p>
-                <p className='unterTitle'><a href={eachObj.url}>{mapTime(eachObj.created_at_i)} </a></p> 
-            
-                
+                  <h1><a  className="title"  href={hit.url}>{hit.title}</a></h1>
+                  <p  className="subHeading" ><a href={hit.url}>{hit.points} points</a> <a href={hit.url}>by {hit.author} </a> <a href={hit.url}>{hit.num_comments} comments</a> <a href={hit.url}>{hit.created_at} </a>
+                  </p>
+                  {/* <p><a href={eachObj.url}>{mapTime(eachObj.created_at_i)} </a></p>  */}
                 </div> 
-                </li>
-               )})} 
-             </ol>
-            </div>
+              </li>
+             )})} 
+           </ol>
+          </div>
         )
-          : <h1>Wait, It's loading.....</h1>
-        }
-    </div>
-     
-  );
+        : <h1>Wait, It's loading.....</h1>
+      }
+    <ReactPaginate
+    previousLabel={"previous"}
+    nextLabel={"next"}
+    breakLabel={"..."}
+    pageCount={pageCount}
+    marginPagesDisplayed={5}
+    onPageChange={handlePageChange}
+    containerClassName={"pagination justify-content-center"}
+    pageClassName={"page-item"}
+    pageLinkClassName= {"page-link"}
+    previousClassName= {"page-items"}
+    previousLinkClassName={"page-link"}
+    nextClassName={"page-item"}
+    nextLinkClassName={"page-link"}
+    breakClassName={"page-item"}
+    breakLinkClassName={"page-Link"}
+    activeClassName={"onPageActive"}
+    
+    />
+    
+    {/* <Footer/> */}
+   
+  </div>
+   
+);
 }
 
 export default App;
