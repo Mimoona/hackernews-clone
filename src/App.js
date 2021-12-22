f3eeb9918c623e475cbec88767d40ebd76999c09
Mimoona <memoona.raheel8@gmail.com>
2021-12-22 10:55:49 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -33,15 +33,6 @@ function App() {
 
 
 
-
-
-
-
-
-
-
-
-
   const handleSubmit = (e) => {
     e.preventDefault()
     setSearch(querie)
@@ -58,33 +49,29 @@ console.log(hits)
           </button>
         </form>
         {loading ? (
-            <div>
+          <div>
 
             <ol >
                 {hits.map(hit => {
-                return(  <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
-
+                return(
+                <li style={{color: "rgb(251, 149, 53)", opacity: "0.8"}}>
                 <div>
-                <h1><a className="title" href={hit.url}>{hit.title}</a></h1>
-                <p className='unterTitle'><a href={hit.url}>{hit.points} points</a></p>
-                <p className='unterTitle'><a href={hit.url}>by {hit.author} </a> </p>
-                <p className='unterTitle'><a href={hit.url}>{hit.num_comments} comments</a></p>
-                <p className='unterTitle'><a href={hit.url}>{hit.created_at} </a></p>
-                <p className='unterTitle'><a href={hit.url}>{mapTime(hit.created_at_i)} </a></p> 
-            
-                
+                  <h1><a className="title" href={hit.url}>{hit.title}</a></h1>
+                  <p className='unterTitle'><a href={hit.url}>{hit.points} points</a></p>
+                  <p className='unterTitle'><a href={hit.url}>by {hit.author} </a> </p>
+                  <p className='unterTitle'><a href={hit.url}>{hit.num_comments} comments</a></p>
+                  <p className='unterTitle'><a href={hit.url}>{hit.created_at} </a></p>
+                  <p className='unterTitle'><a href={hit.url}>{mapTime(hit.created_at_i)} </a></p>                 
                 </div> 
                 </li>
-               )})} 
-             </ol>
-            </div>
-          )
-
-        
-          : <h1>Wait, It's loading.....</h1>
+              )
+              })} 
+            </ol>
+          </div>
+        )
+        : <h1>Wait, It's loading.....</h1>
         }
     </div>
-     
   );
 }
 
